dca7d23588403fcd70896aa7161a1db38eaae593
José Lucena <joselucenamorente@outlook.es>
2018-12-13 23:06:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,11 @@
 
 "use strict";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const restService = express();
 const GIPHY_TOKEN =  process.env.giphy; // Defined as env var on Heroku
-
 restService.use(
   bodyParser.urlencoded({ extended: true })
 );
@@ -25,7 +24,7 @@ restService.post("/webhook",function(req,res){
 	let promise;
 	let tag = req.body.result.action //We can change the tag to get differents gifs 
  	promise = new Promise(function(resolve){
-    	request("https://api.giphy.com/v1/gifs/random?api_key=aCNIGQ4MwC060tFQxUQ8YtNy4zDqQ6kO&tag="+tag+"&rating=PG-13",function(err,res,body){
+    	request("https://api.giphy.com/v1/gifs/random?api_key="+GIPHY_TOKEN+"&tag="+tag+"&rating=PG-13",function(err,res,body){
       		resolve(response = JSON.parse(body).data.images.original.url)
     	});
   	});
