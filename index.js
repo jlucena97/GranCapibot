5af687e949c623e139b7caa28931cac1438ee369
José Lucena <joselucenamorente@outlook.es>
2019-06-05 22:29:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ restService.get("/webhook",function(req,res){
 	let response;
 	let promise;
 	let tag = req.body.result.action //We can change the tag to get differents gifs 
-	console.log("Entra");
  	promise = new Promise(function(resolve){
-    	request("https://api.giphy.com/v1/gifs/random?api_key="+GIPHY_TOKEN+"&tag="+tag+"&rating=PG-13",function(err,res,body){
+    	request("http://api.giphy.com/v1/gifs/random?api_key=fOrbc4xCvjkD29N0UPFtH6E2SCIerdi8&tag=party&rating=PG-13",function(err,res,body){
       		resolve(response = JSON.parse(body).data.images.original.url)
     	});
   	});
